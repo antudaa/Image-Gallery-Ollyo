7837ea75f97222ab81edcb41d5cec43360c2aebe
unknown <antu.haks@gmail.com>
2023-11-02 10:53:14 +0600

    Initalized Git & Added some aditional Changes

---
+++ src/data.js
@@ -0,0 +1,59 @@
+const initialImageList = [
+  {
+    id: 1,
+    title: "Image 1",
+    url: "",
+  },
+  {
+    id: 2,
+    title: "Image 2",
+    url: "",
+  },
+  {
+    id: 3,
+    title: "Image 3",
+    url: "",
+  },
+  {
+    id: 4,
+    title: "Image 1",
+    url: "",
+  },
+  {
+    id: 5,
+    title: "Image 2",
+    url: "",
+  },
+  {
+    id: 6,
+    title: "Image 3",
+    url: "",
+  },
+  {
+    id: 7,
+    title: "Image 1",
+    url: "",
+  },
+  {
+    id: 8,
+    title: "Image 2",
+    url: "",
+  },
+  {
+    id: 9,
+    title: "Image 3",
+    url: "",
+  },
+  {
+    id: 10,
+    title: "Image 1",
+    url: "",
+  },
+  {
+    id: 11,
+    title: "Image 2",
+    url: "",
+  },
+];
+
+export default initialImageList;
